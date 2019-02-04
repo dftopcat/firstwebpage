cce854be16241c8b576892defecdfe37e5af2b8a
HistoryW NetstatLs <dftopcat@gmail.com>
2019-02-04 23:28:56 +0000

    first website

---
+++ js/index.js
@@ -0,0 +1,9 @@
+
+var visitor = prompt('What is your name');
+var message = 'Hello ' + visitor + ", welcome to my HTML, CSS and JS first webpage.";
+document.write(message);
+
+
+function update(){
+    document.getElementById('test').innerHTML="Use page for future reference or pass to anyone starting out as a Web develop. Fix me up and practice.🙂"
+}
